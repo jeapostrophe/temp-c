5af26a39cdd43a2ed18123cec3b0654480c9fa38
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-24 17:20:49 -0500

    Move temp-c tests from unstable repo.

---
+++ temp-c-test/info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define collection "automata")
+(define version "1.0")
+(define deps '(("base" #:version "6.2.900.4")
+               "temp-c-lib"))
+(define build-deps '("eli-tester"
+                     "errortrace-lib"))
+(define pkg-authors '(jay))
