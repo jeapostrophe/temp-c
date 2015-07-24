22deb5a7038a522790405edadfe66ef2b576b494
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-24 16:52:48 -0500

    Add info.rkt file.

--- temp-c-lib/info.rkt
+++ temp-c-lib/info.rkt
@@ -1,3 +1,9 @@
 #lang info
 
+(define collection "temp-c")
+(define version "1.0")
+(define deps '(("base" #:version "6.2.900.4")
+               "automata-lib"))
+(define pkg-authors '(jay))
+
 (define test-responsibles '((all jay)))
