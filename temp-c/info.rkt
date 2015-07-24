36561e63f97927eb303672ed8dda64d724d22812
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-24 17:24:33 -0500

    Add temp-c meta-package.

---
+++ temp-c/info.rkt
@@ -0,0 +1,11 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("temp-c-lib"
+               "temp-c-doc"))
+(define implies '("temp-c-lib"
+                  "temp-c-doc"))
+
+(define version "1.0")
+(define pkg-authors '(jay))
