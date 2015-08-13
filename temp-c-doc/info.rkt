b83a9e6881a767514848f24a8f677c78df368341
Vincent St-Amour <stamourv@racket-lang.org>
2015-08-13 13:49:50 -0500

    Add missing dep.

--- temp-c-doc/info.rkt
+++ temp-c-doc/info.rkt
@@ -5,7 +5,8 @@
 (define deps '(("base" #:version "6.2.900.4")
                "temp-c-lib"))
 (define build-deps '("scribble-lib"
-                     "automata"))
+                     "automata"
+                     "racket-doc"))
 (define pkg-authors '(jay))
 
 (define scribblings '(("scribblings/temp-c.scrbl" () (library))))
